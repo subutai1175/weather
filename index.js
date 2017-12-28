8e1eb8f108994f275c1a42ef4ca879503cd9347e
subutai1175 <subutai1175@gmail.com>
2017-12-29 04:14:00 +0800

    added authentication

--- index.js
+++ index.js
@@ -14,6 +14,29 @@ exports.helloHttp = function helloHttp (req, res) {
   }));
 };
 
+// Imports the Google Cloud client library.
+const Storage = require('@google-cloud/storage');
+
+// Instantiates a client. If you don't specify credentials when constructing
+// the client, the client library will look for credentials in the
+// environment.
+const storage = Storage();
+
+// Makes an authenticated API request.
+storage
+  .getBuckets()
+  .then((results) => {
+    const buckets = results[0];
+
+    console.log('Buckets:');
+    buckets.forEach((bucket) => {
+      console.log(bucket.name);
+    });
+  })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
+  
 const port = 3000
 
 const express = require('express');
