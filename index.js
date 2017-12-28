4daf95cd9eafce7e3abbddb1f140018baadc21a7
subutai1175 <subutai1175@gmail.com>
2017-12-28 22:06:52 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const port = 3000
+
+const express = require('express');
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+})
