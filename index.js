1da0c58801cec0f1dcf2ffb3bd9f60153955f813
subutai1175 <subutai1175@gmail.com>
2017-12-29 11:32:15 +0800

    deployed to gcloud

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
+const express = require('express');
+
+const app = express()
+
 /*
 * HTTP Cloud Function.
 *
 * @param {Object} req Cloud Function request context.
 * @param {Object} res Cloud Function response context.
 */
-exports.helloHttp = function helloHttp (req, res) {
-  response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+// exports.helloHttp = function helloHttp (req, res) {
+//   response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+//
+//
+//   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+//   res.send(JSON.stringify({ "speech": response, "displayText": response
+//   //"speech" is the spoken version of the response, "displayText" is the visual version
+//   }));
+// };
+//
+// // Imports the Google Cloud client library.
+// const Storage = require('@google-cloud/storage');
+//
+// // Instantiates a client. If you don't specify credentials when constructing
+// // the client, the client library will look for credentials in the
+// // environment.
+// const storage = Storage();
+//
+// // Makes an authenticated API request.
+// storage
+//   .getBuckets()
+//   .then((results) => {
+//     const buckets = results[0];
+//
+//     console.log('Buckets:');
+//     buckets.forEach((bucket) => {
+//       console.log(bucket.name);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error('ERROR:', err);
+//   });
 
-
-  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  res.send(JSON.stringify({ "speech": response, "displayText": response
-  //"speech" is the spoken version of the response, "displayText" is the visual version
-  }));
-};
-
-// Imports the Google Cloud client library.
-const Storage = require('@google-cloud/storage');
-
-// Instantiates a client. If you don't specify credentials when constructing
-// the client, the client library will look for credentials in the
-// environment.
-const storage = Storage();
-
-// Makes an authenticated API request.
-storage
-  .getBuckets()
-  .then((results) => {
-    const buckets = results[0];
-
-    console.log('Buckets:');
-    buckets.forEach((bucket) => {
-      console.log(bucket.name);
-    });
-  })
-  .catch((err) => {
-    console.error('ERROR:', err);
-  });
-  
-const port = 3000
-
-const express = require('express');
-
-const app = express()
+const port = process.env.PORT ||3000
 
 app.get("/", (req, res) => {
   res.send("Hello World")
